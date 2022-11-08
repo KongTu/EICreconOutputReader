6245b6017a1094c6f5e078960a90b22a755400b0
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-08 16:55:54 -0500

    update

--- getInputFromS3.sh
+++ getInputFromS3.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 mc config host add S3 https://dtn01.sdcc.bnl.gov:9000 eicS3read eicS3read
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/1GeV/130to177deg/pi-_1GeV_130to177deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-1GeV-backward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/1GeV/3to50deg/pi-_1GeV_3to50deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-1GeV-forward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/1GeV/45to135deg/pi-_1GeV_45to135deg.0001.eicrecon.tree.edm4eic.root ./input/input-pi-1GeV-mid.root
 
 mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/130to177deg/pi-_10GeV_130to177deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-backward.root
 mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/3to50deg/pi-_10GeV_3to50deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-forward.root
@@ -19,3 +16,4 @@ echo "Next step is to run reader on them, for example:"
 echo "************************************************"
 echo "./runSingleParticleReader.sh input/input-pi-10GeV-backward.root pi-10GeV-backward"
 echo "************************************************"
+
