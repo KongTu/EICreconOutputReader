25546ea8c4f4cb3b7c90c1646d3146a7cd2e9ece
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-08 13:39:51 -0500

    update

---
+++ src/pleaseIncludeMe.h
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+#include "ROOT/RDataFrame.hxx"
+#include <TH1D.h>
+#include <TFitResult.h>
+#include <TRandom3.h>
+#include <TCanvas.h>
+
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TLorentzRotation.h"
+#include "TVector2.h"
+#include "TVector3.h"
+
+#include "fmt/color.h"
+#include "fmt/core.h"
+
+// #include "nlohmann/json.hpp"
+#include "edm4eic/InclusiveKinematicsData.h"
+#include "edm4eic/ReconstructedParticleData.h"
+// #include "edm4eic/ReconstructedParticleCollection.h"
+
+#define PI            3.1415926
+#define MASS_ELECTRON 0.00051
+#define MASS_PROTON   0.93827
+#define MASS_PION     0.13957
+#define MASS_KAON     0.493667
+#define MASS_AU197    183.45406466643374
+
+auto getNtrk(const std::vector<edm4eic::ReconstructedParticleData>& parts)
+{
+  std::vector<int> mult;
+  int n=0;
+  for(auto& i1 : parts){
+    if(i1.charge!=0) n++;
+  }
+  mult.push_back( n );
+  return mult;
+}
+
+auto getEta(const std::vector<edm4eic::ReconstructedParticleData>& parts)
+{
+	std::vector<double> eta;
+	for(auto& i; parts){
+		TVector3 trk(i.momentum.x,i.momentum.y,i.momentum.z);
+		if(i1.charge!=0) eta.push_back(trk.Eta());
+	}
+	return eta;
+}
--- src/readSingleParticles.cxx
+++ src/readSingleParticles.cxx
@@ -1,52 +1,22 @@
-#include <cmath>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <utility>
-
-#include "ROOT/RDataFrame.hxx"
-#include <TH1D.h>
-#include <TFitResult.h>
-#include <TRandom3.h>
-#include <TCanvas.h>
-
-#include "TFile.h"
-#include "TLorentzVector.h"
-#include "TLorentzRotation.h"
-#include "TVector2.h"
-
-//#include "nlohmann/json.hpp"
-//#include "edm4eic/InclusiveKinematicsData.h"
-#include "edm4eic/ReconstructedParticleData.h"
-
-auto getNtrk(const std::vector<edm4eic::ReconstructedParticleData>& parts)
-{
-  std::vector<int> mult;
-  int n=0;
-  for(auto& i1 : parts){
-    if(i1.charge!=0) n++;
-  }
-  mult.push_back( n );
-  return mult;
-}
-
-int readSingleParticles(TString inname="input/pi-_2GeV_130to177deg.0001.eicrecon.tree.edm4eic.root",TString outname="test"){
-	
+#include "pleaseIncludeMe.h"
+int readSingleParticles(TString inname="input/input.root",TString outname="test"){
 	
  	TString rec_file=inname;
 	//ROOT::EnableImplicitMT(kNumThreads);
 	ROOT::RDataFrame d("events", rec_file);
 
-	auto d1 = d.Define("mult",getNtrk,{"ReconstructedChargedParticles"});
+	auto d1 = d.Define("mult",getNtrk,{"ReconstructedChargedParticles"})
+						 .Define("eta",getEta,{"ReconstructedChargedParticles"})
 	auto h_mult_REC = d1.Histo1D({"h_mult_REC", "; N; counts", 10, -0.5, 9.5}, "mult");
+	auto h_eta_REC = d1.Histo1D({"h_eta_REC", "; N; counts", 100, -5, 5}, "eta");
 
 	TString output_name_dir = outname;
-    TFile* output = new TFile("output/"+output_name_dir+"-output.root","RECREATE");
+  TFile* output = new TFile("output/"+output_name_dir+"-output.root","RECREATE");
 	h_mult_REC->Write();
+	h_eta_REC->Write();
+	
 	output->Write();
-    output->Close();
+  output->Close();
 
 	return 0;
 }
