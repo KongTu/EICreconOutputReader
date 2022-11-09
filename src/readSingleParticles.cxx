064aace695c0799280939635f97d7563c37f0611
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-09 15:01:33 -0500

    update generator status code

--- src/readSingleParticles.cxx
+++ src/readSingleParticles.cxx
@@ -16,11 +16,12 @@ int readSingleParticles(TString inname="input/input.root",TString outname="test"
 						 .Define("etaMC",getEta,{"momentumMC"})
 						 .Define("ptMC",getPt,{"momentumMC"})
 						 .Define("phiMC",getPhi,{"momentumMC"})
+						 .Define("pidProbMC",getPIDprob,{"momentumMC"})
 						 .Define("ptRes",pt_resolution,{"MCParticles","ReconstructedChargedParticles"})
 						 ;
 
 	TString output_name_dir = outname;
-  TFile* output = new TFile("output/"+output_name_dir+"-output.root","RECREATE");
+  	TFile* output = new TFile("output/"+output_name_dir+"-output.root","RECREATE");
 
 	auto h_mult_REC = d1.Histo1D({"h_mult_REC", "; N; counts", 10, -0.5, 9.5}, "mult");
 	auto h_mult_MC = d1.Histo1D({"h_mult_MC", "; N; counts", 10, -0.5, 9.5}, "multMC");
@@ -34,6 +35,7 @@ int readSingleParticles(TString inname="input/input.root",TString outname="test"
 	auto h_pt_Res = d1.Histo1D({"h_pt_Res", "; Resolution; counts", 100, -1,1}, "ptRes");
 	auto h_pt_Res2D = d1.Histo2D({"h_pt_Res2D", "; p_{T} (GeV/c); Resolution",100,0,5,100,-1,1},"pt","ptRes");
 	auto h_etaVsPIDprob = d1.Histo2D({"h_etaVsPIDprob", "; #eta; PID probability",100,-5, 5,100,0,1},"eta","pidProb");
+	auto h_etaVsPIDprobMC = d1.Histo2D({"h_etaVsPIDprobMC", "; #eta; PID probability",100,-5, 5,100,0,1},"etaMC","pidProbMC");
 
 	//MC
 	h_mult_MC->Write();
@@ -51,9 +53,10 @@ int readSingleParticles(TString inname="input/input.root",TString outname="test"
 	//pid
 	h_pidProb_REC->Write();
 	h_etaVsPIDprob->Write();
+	h_etaVsPIDprobMC->Write();
 
 	output->Write();
-  output->Close();
+  	output->Close();
 
 	return 0;
 }
