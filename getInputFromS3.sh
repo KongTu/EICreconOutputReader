5f5b1c7e81f60e38964cb96b97695104ff6e4880
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-09 14:01:59 -0500

    update

--- getInputFromS3.sh
+++ getInputFromS3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mc config host add S3 https://dtn01.sdcc.bnl.gov:9000 eicS3read eicS3read
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/130to177deg/pi-_10GeV_130to177deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-backward.root
+mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/130to177deg/pi-_10GeV_130to177deg.0006.eicrecon.tree.edm4eic.root ./input/input-eicrecon-pi-10GeV-backward.root
+mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/130to177deg/pi-_10GeV_130to177deg.0006.juggler.tree.edm4eic.root ./input/input-juggler-pi-10GeV-backward.root
 
 echo "************************************************"
 echo "Download from S3 successful."
