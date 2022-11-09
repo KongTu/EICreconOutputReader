3b1d8d7c5a3bdb697de29d857654228e9fc3965d
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-09 13:25:37 -0500

    update output and scripts

--- getInputFromS3.sh
+++ getInputFromS3.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 mc config host add S3 https://dtn01.sdcc.bnl.gov:9000 eicS3read eicS3read
-
 mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/130to177deg/pi-_10GeV_130to177deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-backward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/3to50deg/pi-_10GeV_3to50deg.0006.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-forward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/pi-/10GeV/45to135deg/pi-_10GeV_45to135deg.0001.eicrecon.tree.edm4eic.root ./input/input-pi-10GeV-mid.root
-
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/e-/10GeV/130to177deg/e-_10GeV_130to177deg.0001.eicrecon.tree.edm4eic.root ./input/input-e-10GeV-backward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/e-/10GeV/3to50deg/e-_10GeV_3to50deg.0001.eicrecon.tree.edm4eic.root ./input/input-e-10GeV-forward.root
-mc cp --insecure S3/eictest/EPIC/RECO/22.10.0/epic_brycecanyon/SINGLE/e-/10GeV/45to135deg/e-_10GeV_45to135deg.0001.eicrecon.tree.edm4eic.root ./input/input-e-10GeV-mid.root
 
 echo "************************************************"
 echo "Download from S3 successful."
