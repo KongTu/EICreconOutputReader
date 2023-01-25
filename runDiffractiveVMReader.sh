84eadf01c5740df239cb35502ff69a91b34bd7f3
Kong Tu <kongkong@rice.edu>
2023-01-25 09:55:06 -0500

    update run script

--- runDiffractiveVMReader.sh
+++ runDiffractiveVMReader.sh
@@ -2,11 +2,13 @@
 
 input=$1
 output=$2
+VM=$3
 
 echo "Running analysis"
 echo "Input edm4eic data is at [ ${input} ] "
 echo "Output at [ ${output} ]"
+echo "VM species is [ ${VM} ]"
 
-root -b -q src/diffractive_vm_simple_analysis.cxx+\(\"${input}\",\"${output}\"\)
+root -b -q src/diffractive_vm_simple_analysis.cxx+\(\"${input}\",\"${output}\",${VM}\)
 
 
