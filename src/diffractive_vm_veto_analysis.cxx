7618963e8996bb35ae99793973cf2c6ef1b338ee
Zhoudunming Tu <kong@130-199-16-231.dhcp.bnl.gov>
2023-07-13 10:28:23 -0400

    adding new codes

--- src/diffractive_vm_veto_analysis.cxx
+++ src/diffractive_vm_veto_analysis.cxx
@@ -44,9 +44,6 @@ TTreeReaderArray<float> emhits_x_array = {tree_reader, "EcalEndcapNRecHits.posit
 TTreeReaderArray<float> emhits_y_array = {tree_reader, "EcalEndcapNRecHits.position.y"};
 TTreeReaderArray<float> emhits_energy_array = {tree_reader, "EcalEndcapNRecHits.energy"};
 
-TTreeReaderArray<unsigned int> em_rec_id_array = {tree_reader, "EcalEndcapNClustersAssociations.recID"};
-TTreeReaderArray<unsigned int> em_sim_id_array = {tree_reader, "EcalEndcapNClustersAssociations.simID"};
-
 // Reconstructed particles pz array for each reconstructed particle
 TTreeReaderArray<float> reco_px_array = {tree_reader, "ReconstructedChargedParticles.momentum.x"};
 TTreeReaderArray<float> reco_py_array = {tree_reader, "ReconstructedChargedParticles.momentum.y"};
