b928d6bb79b8dc12a22ea71b213fea76c45a6551
Zhoudunming Tu <kong@130-199-16-231.dhcp.bnl.gov>
2023-07-14 11:30:42 -0400

    update scripts

--- runDiffractiveVMVeto.sh
+++ runDiffractiveVMVeto.sh
@@ -2,9 +2,11 @@
 
 input=$1
 output=$2
+veto=$3
 
 echo "Running analysis"
 echo "Input edm4eic data is at [ ${input} ] "
 echo "Output at [ ${output} ]"
+echo "Are we vetoing? " ${veto}
 
-root -b -q src/diffractive_vm_veto_analysis.cxx+\(\"${input}\",\"${output}\"\)
+root -b -q src/diffractive_vm_veto_analysis.cxx+\(\"${input}\",\"${output}\"\,${veto}\)
