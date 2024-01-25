cd3443a7c6f5b3682bcc2f3f9954071e66cf17c1
Zhoudunming Tu <kong@130-199-16-231.dhcp.bnl.gov>
2024-01-25 10:36:58 -0500

    script to run

---
+++ runPhotoproductionVM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input=$1
+output=$2
+veto=$3
+
+echo "Running analysis"
+echo "Input edm4eic data is at [ ${input} ] "
+echo "Output at [ ${output} ]"
+echo "Are we vetoing? " ${veto}
+
+root -b -q src/photoproduction_vm_analysis.cxx+\(\"${input}\",\"${output}\"\,${veto}\)
